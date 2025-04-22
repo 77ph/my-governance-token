bc0900108aa6e2e64bf84ee189e2ad985d3644c9
AL <info@77ph.net>
2025-04-22 14:15:40 +0100

    feat: moving to V2

--- src/MyGovernanceToken.sol
+++ src/MyGovernanceToken.sol
@@ -1,69 +1,80 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-//import "./GovernanceOFTV2.sol";
-
-import "solidity-examples/token/oft/v2/OFTV2.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import { OFT } from "@layerzerolabs/oft-evm/contracts/OFT.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
-//contract MyGovernanceToken is GovernanceOFTV2, ERC20Votes, Ownable {
-contract MyGovernanceToken is Ownable, OFTV2, ERC20Permit, ERC20Votes {
-    uint8 private immutable _sharedDecimals;
+struct SendParam {
+     uint32 dstEid; // Destination endpoint ID.
+     bytes32 to; // Recipient address.
+     uint256 amountLD; // Amount to send in local decimals.
+     uint256 minAmountLD; // Minimum amount to send in local decimals.
+     bytes extraOptions; // Additional options supplied by the caller to be used in the LayerZero message.
+     bytes composeMsg; // The composed message for the send() operation.
+     bytes oftCmd; // The OFT command to be executed, unused in default OFT implementations.
+}
 
+struct MessagingFee {
+    uint nativeFee; // gas amount in native gas token
+    uint lzTokenFee; // gas amount in ZRO token
+}
+
+contract MyGovernanceToken is OFT, ERC20Votes {
     constructor(
         string memory name_,
         string memory symbol_,
         address lzEndpoint_,
-        uint8 sharedDecimals_,
         address initialOwner
     )
-        OFTV2(name_, symbol_, sharedDecimals_, lzEndpoint_)
+
+        OFT(name_, symbol_, lzEndpoint_, initialOwner) 
         Ownable(initialOwner)
-        ERC20Permit(name_)
+        ERC20Votes()
     {
-        _sharedDecimals = sharedDecimals_;
         _mint(initialOwner, 1_000_000 * 10 ** decimals());
     }
 
+    // Mint только на L1
     function mint(address to, uint256 amount) external onlyOwner {
         require(block.chainid == 1, "Mint only on L1");
         _mint(to, amount);
     }
 
+    // Burn только свои токены
     function burn(uint256 amount) external {
         _burn(msg.sender, amount);
     }
 
+    // Можно также добавить безопасную отправку с минимальным лимитом
     function safeSendFrom(
-        address from,
-        uint16 dstChainId,
-        bytes calldata toAddress,
-        uint256 amount,
-        address payable refundAddress,
-        address zroPaymentAddress,
-        bytes calldata adapterParams
+        SendParam calldata _sendParam,
+        MessagingFee calldata _fee,
+        address _refundAddress
     ) external payable {
-        uint256 minTransferable = 10 ** (decimals() - _sharedDecimals);
-        require(amount >= minTransferable, "Too small");
-
-        sendFrom(
-            from,
-            dstChainId,
-            toAddress,
-            amount,
-            refundAddress,
-            zroPaymentAddress,
-            adapterParams
-        );
+        require(_sendParam.amountLD >= 10 ** 10, "Too small"); // 1e10 = 0.00000001 токена (пример)
+        _send(_sendParam, _fee, _refundAddress);
     }
 
-    function _update(address from, address to, uint256 value)
+    // Vote tracking
+    function _afterTokenTransfer(address from, address to, uint256 amount)
         internal
         override(ERC20, ERC20Votes)
     {
-        super._update(from, to, value);
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _mint(address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._mint(to, amount);
+    }
+
+    function _burn(address from, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._burn(from, amount);
     }
 }
-
